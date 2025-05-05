babdd2dcf50bb06f8ac9ca9edaaa88e498d1d25a
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2025-05-05 15:30:24 +0300

    Add transaction input fields and FloatingActionButton for navigation

--- app/src/main/java/com/oguzhanozgokce/lab6_0505_marmara/MainActivity.java
+++ app/src/main/java/com/oguzhanozgokce/lab6_0505_marmara/MainActivity.java
@@ -1,6 +1,8 @@
 package com.oguzhanozgokce.lab6_0505_marmara;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        fabAdd = findViewById(R.id.fab_add);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
